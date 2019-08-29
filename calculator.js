ab56082d5b6a009217e4d68b3f309005b02f323e
adam-weiler <adam.weiler@yahoo.ca>
2019-08-29 14:13:52 -0400

    Sum function is passing tests.

--- calculator.js
+++ calculator.js
@@ -6,7 +6,8 @@ function subtract(num1, num2) {
     return num1 - num2;
 }
 
-function sum() {
+function sum(arr) {
+    return arr.reduce((total, num) => total + num, 0);
 }
 
 module.exports = {add, subtract, sum}
